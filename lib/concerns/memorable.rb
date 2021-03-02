33463071ac010f4c27895226a176ae59d4dbc183
PepperMcLean <jackmcl206@gmail.com>
2021-03-02 16:16:30 -0600

    commit tree e4c8941def4a972a5330dcc6ff456a818dd9c9d7

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,30 @@
+module Memorable
+
+    module ClassMethods
+        
+        def reset_all
+            self.all.clear
+        end
+
+        def count
+            self.all.count
+        end
+
+    end
+
+    module InstanceMethods
+        def initialize
+           self.class.all << self
+        end
+    end
+end 
+
+class Song
+    extend Memorable::ClassMethods
+    include Memorable::InstanceMethods
+end
+
+class Artist
+    extend Memorable::ClassMethods
+    include Memorable::InstanceMethods
+end
